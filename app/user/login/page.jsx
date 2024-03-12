497267694090b66626b4018acddb295d9a1c6c33
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-03-12 09:07:49 +0500

    base

--- app/user/login/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function page() {
-  return <div>login page</div>;
-}
-
-export default page;
--- app/user/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function page() {
-  return <div>user page</div>;
-}
-
-export default page;
