34125a0929345ae5bbcd508c5224635bdad40ea8
Arslan Iftikhar <a44222666@gmail.com>
2024-03-14 05:49:22 +0500

    Update page.js

--- app/(user)/register/page.js
+++ app/(user)/register/page.js
@@ -1,18 +1,33 @@
 "use client";
+import "@/styles/login.css";
 
 import { GoogleLogin } from "@react-oauth/google";
 import { registerWithGoogle } from "@/actions/auth";
+import Link from "next/link";
 
-export default function login() {
+export default function Register() {
   return (
-    <div className="flex">
-      register
-      <GoogleLogin
-        onSuccess={registerWithGoogle}
-        onError={() => {
-          console.log("Login Failed");
-        }}
-      />
+    <div className="container__main">
+      <div className="container__main__login">
+        <div className="container__main__login__heading">Register</div>
+        <div className="container__main__login__text">
+          Register your account.
+        </div>
+        <div className="container__main__login__google">
+          <GoogleLogin
+            shape="pill"
+            onSuccess={(credentialResponse) => {
+              console.log(credentialResponse);
+            }}
+            onError={() => {
+              console.log("Login Failed");
+            }}
+          />
+        </div>
+        <div className="container__main__login__text">
+          Already have an account? <Link href={"login"}>Login</Link>
+        </div>
+      </div>
     </div>
   );
 }
